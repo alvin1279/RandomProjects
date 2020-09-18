88f002b96f2f7c7a3b4c71a3e1ed06611a0baba4
alvin1279 <35830168+alvin1279@users.noreply.github.com>
2020-09-18 23:22:32 +0530

    completely removed precalculated points
    adding resol atribute should have corrected the speed to be same for each radius for same speed value, previously different radius had differrent speed for same speed value

--- Revolving_balls.py
+++ Revolving_balls.py
@@ -14,23 +14,21 @@ class Ball:
     speed   → No. of points  per frame
     x,y     → Anchor point or point about revolution
     x1,y1   → Position of ball
-    Theta   → Angles
-    Point   → [cos,sin] points about 3600, precalculated
+    resol   → increases the total number of points, increased for larger radius(int(radius/4))
+    Theta   → Total no of points, for each object depends on its resol
+    self.Point   → [cos,sin] 
     """
     x = y = 0
-
-    # Pre calculation points
-    theta = np.arange(0, 360, 0.1)
-    sin = np.sin((theta / 180) * np.pi)
-    cos = np.cos((theta / 180) * np.pi)
-    Point = [cos, sin]
 
     def __init__(self, radius=1, speed=1, cl=True, trace=False):
         global rmax
         self.r = radius
         self.speed = speed
         self.cl = cl
-        self.scat = ax.scatter([], [], c='r', s=100, zorder=2)
+        
+        self.scat = ax.scatter([], [], c='r', s=100, zorder=2)     
+        self.resol = int(self.r/4)
+        self.calc_points(self.resol)
         self.rev_points()
         self.trace(trace)
 
@@ -43,7 +41,7 @@ class Ball:
 
         if trace:
             trace_step = int(500 / self.r)
-            trace_points = Ball.Point[0][::trace_step], Ball.Point[1][::trace_step]
+            trace_points = self.Point[0][::trace_step], self.Point[1][::trace_step]
             ax.scatter(trace_points[0] * self.r, trace_points[1] * self.r, s=1)
 
     def plot(self):
@@ -55,15 +53,16 @@ class Ball:
         return self.scat
 
     def rev_points(self):
-        rev_step = int(100 * self.speed / self.r)
-        self.rev_point = cycle(Ball.Point[0][::rev_step]), cycle(Ball.Point[1][::rev_step])
+        rev_step = self.speed * 10 * self.resol
+        self.rev_point = cycle(self.Point[0][::rev_step]), cycle(self.Point[1][::rev_step])
         
-    def calcpoints(resol=1)
+    def calc_points(self,resol=1):
+        
         step = 0.1
         theta = np.arange(0, 360, step/resol)
         sin = np.sin((theta / 180) * np.pi)
         cos = np.cos((theta / 180) * np.pi)
-        self.Points = [cos, sin]
+        self.Point = [cos, sin]
 
 # Plot Initialisation,write this as an class function
 rmax = 20
